8ede714c7ac953b84aaed2b05521bd88e5bf5a35
Doyun <doyun@talktomeinkorean.com>
2023-06-21 22:06:03 +0900

    feature : getSpreadSheetData -> async

--- backend/getSpreadSheetData.ts
+++ backend/getSpreadSheetData.ts
@@ -2,22 +2,26 @@ import {google} from "googleapis";
 import connection from "./database";
 const keys = require("./keys.json");
 
-const client = new google.auth.JWT(
-  keys.client_email,
-  undefined,
-  keys.private_key,
-  ["https://www.googleapis.com/auth/spreadsheets"]
-);
+const getSpreadSheetData = async () => {
+  const client = new google.auth.JWT(
+    keys.client_email,
+    undefined,
+    keys.private_key,
+    ["https://www.googleapis.com/auth/spreadsheets"]
+  );
 
-client.authorize((err: Error | null, tokens: any) => {
-  if (err) {
-    console.error(err);
-    return;
-  } else {
-    console.log("Connected to Google Sheets API!");
-    getData(client);
-  }
-});
+  return new Promise((resolve, reject) => {
+    client.authorize((err: Error | null, tokens: any) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log("Connected to Google Sheets API!");
+        getData(client).then(resolve).catch(reject);
+      }
+    });
+  });
+};
 
 const getData = (client: any) => {
   const sheets = google.sheets({
@@ -29,28 +33,37 @@ const getData = (client: any) => {
     range: "voca_app!A1:B10",
   };
 
-  sheets.spreadsheets.values.get(options, (err: any, res: any) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Data:", res.data.values);
-      insertDataIntoDatabase(res.data.values);
-    }
+  return new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.get(options, (err: any, res: any) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log("Data:", res.data.values);
+        insertDataIntoDatabase(res.data.values);
+        resolve(res.data.values);
+      }
+    });
   });
 };
 
 const insertDataIntoDatabase = (data: any[]) => {
-  const query = "INSERT INTO quiz (question, answer) VALUES ?";
+  const query =
+    "INSERT INTO quiz (question, answer) VALUES (?, ?) ON DUPLICATE KEY UPDATE question=VALUES(question), answer=VALUES(answer);";
 
-  connection.query(
-    query,
-    [data],
-    (err: any, result: {affectedRows: string}) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log("Number of records inserted: " + result.affectedRows);
+  data.forEach(([question, answer]) => {
+    connection.query(
+      query,
+      [question, answer],
+      (err: any, result: {affectedRows: string}) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Number of records inserted: " + result.affectedRows);
+        }
       }
-    }
-  );
+    );
+  });
 };
+
+module.exports = getSpreadSheetData;
